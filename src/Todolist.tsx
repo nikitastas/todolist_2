ee72a5e17cefaf829418fee83654a2ae52284e39
Nikita Stasiulevich <nikitastas1694@gmail.com>
2024-06-13 01:13:19 +0300

    first commit

---
+++ src/Todolist.tsx
@@ -0,0 +1,34 @@
+import {TaskType} from "./App";
+import {Button} from "./Button";
+
+type PropsType = {
+	title: string
+	tasks: TaskType[]
+}
+
+export const Todolist = ({title, tasks}: PropsType) => {
+
+	return (
+		<div>
+			<h3>{title}</h3>
+			<div>
+				<input/>
+				<Button title={'+'}/>
+			</div>
+			{
+				tasks.length === 0
+					? <p>Тасок нет</p>
+					: <ul>
+						{tasks.map((task) => {
+							return <li key={task.id}><input type="checkbox" checked={task.isDone}/><span>{task.title}</span></li>
+						})}
+					</ul>
+			}
+			<div>
+				<Button title={'All'}/>
+				<Button title={'Active'}/>
+				<Button title={'Completed'}/>
+			</div>
+		</div>
+	)
+}
