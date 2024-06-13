e7c547689dc035e5cf0356ea3a68f0fe9c364fa2
Nikita Stasiulevich <nikitastas1694@gmail.com>
2024-06-13 13:30:51 +0300

    Add removeTask function

--- src/App.tsx
+++ src/App.tsx
@@ -17,14 +17,10 @@ function App() {
 		{ id: 5, title: 'Typescript', isDone: false },
 		{ id: 6, title: 'RTK query', isDone: false },
 	])
-	/*const tasks: Array<TaskType> = [
-		{ id: 1, title: 'HTML&CSS', isDone: true },
-		{ id: 2, title: 'JS', isDone: true },
-		{ id: 3, title: 'ReactJS', isDone: false },
-		{ id: 4, title: 'Redux', isDone: false },
-		{ id: 5, title: 'Typescript', isDone: false },
-		{ id: 6, title: 'RTK query', isDone: false },
-	]*/
+
+	const removeTask = (taskId: number) => {
+		setTasks(tasks.filter(f => f.id !==taskId))
+	}
 
 
 	return (
